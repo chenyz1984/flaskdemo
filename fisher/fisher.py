2caeeecc2f28c6738cc29d4d26cc18998d704ecd
unknown <chenyingzi@sciclife.com>
2019-11-05 17:35:53 +0800

    flask02

--- fisher/fisher.py
+++ fisher/fisher.py
@@ -7,17 +7,24 @@
  # Description: 文件描述
  # FilePath: \flaskdemo\fisher\fisher.py
 '''
-from flask import Flask
+from flask import Flask,make_response
 app = Flask(__name__)
 
-@app.route('/hello/<int:n>')
-def hello(n):
-    retstr = ''
-    for i in range(n):
-        retstr = retstr + '<li>' + str(i)+'-'+chr(i) + '</li>'
-    return '<h1>Hello World</h1> '+retstr
+@app.route('/book/search/<q>/<page>')
+def search(q,page):
+    '''q page'''
+    #isbn isbn13 13个0-9纯数字
+    #isbn10 10个0-9数字组成，含有‘-’
+    isbn_or_key = 'key'
+    if len(q) == 13 and q.isdigit():    
+        isbn_or_key = 'isbn'
+    short_q = q.replace('-','')
+    if '-' in q and len(shot_q) == 10 and short_q.isdigit():
+        isbn_or_key = 'isbn'
+    pass
 
 
 if __name__ == '__main__':
-    app.debug = False
+    app.debug = True
+    
     app.run(host='0.0.0.0',debug=True)
