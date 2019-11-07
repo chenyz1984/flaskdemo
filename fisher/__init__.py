33691b5773bd2bc08c3735dc0b50a070625579fb
unknown <chenyingzi@sciclife.com>
2019-11-07 16:33:34 +0800

    添加mysql数据库连接

--- fisher/__init__.py
+++ fisher/__init__.py
@@ -1,22 +1,23 @@
 # -*- coding:utf-8 -*-
 '''
- # Author: ChenYingzi
- # Date: 2019-11-05 23:15:25
- # LastEditors: ChenYingzi
- # LastEditTime: 2019-11-06 20:08:10
- # Description: 文件描述
+ # @Author: ChenYingzi
+ # @Date: 2019-11-06 09:03:28
+ # @LastEditors: ChenYingzi
+ # @LastEditTime: 2019-11-07 15:18:24
+ # @Description: 文件描述
 '''
 
 from flask import Flask
-
+from app.models.book import db
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.secure')
     app.config.from_object('app.setting')
     register_blueprint(app)
+    db.init_app(app)
+    db.create_all(app=app)
     return app
-
 
 def register_blueprint(app):
     from app.web import web
--- fisher/app/secure.py
+++ fisher/app/secure.py
@@ -1,9 +1,21 @@
 # -*- coding:utf-8 -*-
 '''
- # Author: ChenYingzi
- # Date: 2019-11-05 23:29:17
- # LastEditors: ChenYingzi
- # LastEditTime: 2019-11-05 23:29:30
- # Description: 文件描述
+ # @Author: ChenYingzi
+ # @Date: 2019-11-07 09:28:48
+ # @LastEditors: ChenYingzi
+ # @LastEditTime: 2019-11-07 16:31:13
+ # @Description: 文件描述
 '''
+# -*- coding:utf-8 -*-
+'''
+ # @Author: ChenYingzi
+ # @Date: 2019-11-07 09:28:48
+ # @LastEditors: ChenYingzi
+ # @LastEditTime: 2019-11-07 15:22:43
+ # @Description: 文件描述
+'''
+
 DEBUG=True
+SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:root@127.0.0.1:3306/fisher'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_COMMIT_TEARDOWN = True
